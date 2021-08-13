f76a5411b63dce876c3ca8923587c203b13b2d82
yuliya-karimova <y.o.karimova@gmail.com>
2021-08-13 15:05:53 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let reversed = 0;
+    n = Math.abs(n);
+    while(n) {
+        reversed = reversed * 10 + n % 10;
+        n = Math.floor(n / 10);
+    }
+    return reversed;
 }
